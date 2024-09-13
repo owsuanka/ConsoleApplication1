701ee0e72e1ca95bd91d1f9d3da2637206dfead3
234864 <234864@KB37-309-C03>
2024-09-13 11:39:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int int1 = 15;
+	int int2 = 20;
+	int int3 = 20;
+	int int4 = 15;
+
+	if (int3 == int2)
+	{
+		cout << "operator == " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator == " << "false" << endl;
+	}
+
+	if (int1 != int2)
+	{
+		cout << "operator != " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator != " << "false" << endl;
+	}
+
+	if (int3 == int2 && int4 == int1)
+	{
+		cout << "operator == && == " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator == && == " << "false" << endl;
+	}
+
+	if (int3 == int2 || int3 == int1)
+	{
+		cout << "operator == || == " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator == || == " << "false" << endl;
+	}
+
+	if (int4 < int3)
+	{
+		cout << "operator < " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator < " << "false" << endl;
+	}
+
+	if (int2 > int1)
+	{
+		cout << "operator > " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator > " << "false" << endl;
+	}
+
+	if (int3 <= int2)
+	{
+		cout << "operator <= " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator <= " << "false" << endl;
+	}
+
+	if (int3 >= int1)
+	{
+		cout << "operator >= " << "true" << endl;
+
+	}
+	else
+	{
+		cout << "operator >= " << "false" << endl;
+	}
+}
